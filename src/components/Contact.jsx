3a07a3d5b753a4e3b6771c304b8a85bb8f338ab9
abimael.tavares <abimael.tavares@devsquad.com>
2023-08-27 12:28:44 -0300

    adding a contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'Abimael Tavares',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'tavares@abimael.dev.br',
           message: form.message
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
